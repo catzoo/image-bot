3787aa500a84c10a2309c2ebf6840142961aced3
catzoo <34617626+catzoo@users.noreply.github.com>
2019-11-28 16:21:55 -0600

    Added info command, Changed version to 1.0

--- bot.py
+++ bot.py
@@ -2,14 +2,16 @@
 created by catzoo
 created on: 11/9/2019
 """
-__version__ = '0.1.0'
+__version__ = '1.0'
 
 import os
+from datetime import datetime
 import discord
 from discord.ext import commands
 import env_config
 
 bot = commands.Bot(command_prefix='?')
+started = datetime.now()
 
 # loading all the extensions
 extension_list = os.listdir('cog')
@@ -41,7 +43,47 @@ async def correct_guild_only(ctx):
 
 @bot.event
 async def on_ready():
+    # since the bot is ready, this means the bot has started
+    global started
+    started = datetime.now()
+
     print(f'Logged in as {bot.user.name}\nVersion: {__version__}')
 
+
+@bot.command()
+async def info(ctx):
+    date_string = ''
+    time_up = datetime.now() - started
+    time_up = time_up.total_seconds()
+    # grabbing minutes from seconds
+    minutes = int(time_up / 60)
+    time_up -= minutes*60
+    time_up = int(time_up)
+    # grabbing hours from minutes
+    hours = int(minutes / 60)
+    minutes -= hours*60
+    # grabbing days from hours
+    days = int(hours / 24)
+    hours -= days*24
+    # adding it all to the string
+    if time_up != 0:
+        date_string = f' {time_up} seconds' + date_string
+
+    if minutes != 0:
+        date_string = f' {minutes} minutes' + date_string
+
+    if hours != 0:
+        date_string = f' {hours} hours' + date_string
+
+    if days != 0:
+        date_string = f' {days} days' + date_string
+
+    embed = discord.Embed(color=discord.Color.blue())
+    embed.set_thumbnail(url=ctx.me.avatar_url_as())
+    embed.add_field(name='Created by:', value='catzoo', inline=False)
+    embed.add_field(name='Created date:', value='11/9/2019', inline=False)
+    embed.add_field(name='Version:', value=__version__, inline=False)
+    embed.add_field(name='Been up for:', value=date_string)
+    await ctx.send(embed=embed)
 
 bot.run(env_config.token)
